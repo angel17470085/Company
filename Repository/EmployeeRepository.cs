16fa491c32208ab9ce07242fc05528fb0fbf47f0
angel17470085 <17470085@campeche.tecnm.mx>
2023-02-10 01:51:19 -0600

    Chap 16: Paging (Completed)

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Entities.RequestFeatures;
 namespace Repository
 {
     public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
@@ -16,11 +16,19 @@ namespace Repository
         {
         }
 
-        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) => 
-            await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+        public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters,bool trackChanges)
+        {
+           var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
             .OrderBy(e => e.Name)
+            .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
+            .Take(employeeParameters.PageSize)
             .ToListAsync();
+            
+            var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).CountAsync();
 
+            return new PagedList<Employee>(employees, employeeParameters.PageNumber, employeeParameters.PageSize, count);
+        } 
+          
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) => 
             await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
             .SingleOrDefaultAsync();
